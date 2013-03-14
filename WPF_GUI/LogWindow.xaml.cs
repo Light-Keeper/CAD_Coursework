bc2bec6f53d5a1843e934d465b185509f69aa96c
Astron <astr0n@ukr.net>
2013-03-14 12:34:44 +0200

    Added buffer for LogViewer

--- WPF_GUI/LogWindow.xaml.cs
+++ WPF_GUI/LogWindow.xaml.cs
@@ -15,19 +15,50 @@ using System.Windows.Shapes;
 
 namespace WPF_GUI
 {
-    /// <summary>
-    /// Interaction logic for LogWindow.xaml
-    /// </summary>
     public partial class LogWindow : Window
     {
+        public struct LogInfo
+        {
+            public DateTime Time;
+            public String Msg;
+        }
+
+        private static readonly int MAX_MSG_IN_BUFFER = 200;
+
+        private static readonly List<LogInfo> Buffer = new List<LogInfo>();
+
         public LogWindow()
         {
             InitializeComponent();
+            foreach (var log in Buffer)
+            {
+                LogViewer.Text += FormLogMsg(log);
+            }
+        }
+
+        public static string FormLogMsg(LogInfo logInfo)
+        {
+            return "[" + logInfo.Time.ToLongTimeString() + "]: " + logInfo.Msg + "\n";
         }
 
         public void Print(string str)
         {
-            LogViewer.Text += "> " + str + "\n";
+            var newLog = new LogInfo
+                {
+                    Msg = str,
+                    Time = DateTime.Now
+                };
+
+            Buffer.Add(newLog);
+
+            // Pushing new log to console
+            LogViewer.Text += FormLogMsg(newLog);
+
+            // Delete first output log
+            if (Buffer.Count > MAX_MSG_IN_BUFFER)
+            {
+                Buffer.RemoveAt(0);
+            }
         }
 
         private void LogWindow_OnClosing(object sender, CancelEventArgs e)
