431f192c2e11f802cb884f81b33564a64e154ff1
Янушкевич Вадим <yanushkevich@trueconf.com>
2013-03-14 15:28:02 +0200

    add project Kraveckiy

---
+++ Kraveckiy/Kraveckiy.cpp
@@ -0,0 +1,22 @@
+#include <cad_module.h>
+#include <cad_object.h>
+
+cad_route_map *Open(cad_kernel *c, cad_route_map *m);
+cad_route_map *Close(cad_kernel *c, cad_route_map *m);
+
+cad_module_begin()
+	set_module_name( "Трассировка двумя волнами. Кравецкий" )
+	set_module_priority( 0 )
+	set_module_capability( CAP_TRACEROUTE )
+	set_module_callbacks(Open, Close)
+cad_module_end()
+
+cad_route_map *Open(cad_kernel *c, cad_route_map *m)
+{
+	return NULL;
+}
+
+cad_route_map *Close(cad_kernel *c, cad_route_map *m)
+{
+	return NULL;
+}
