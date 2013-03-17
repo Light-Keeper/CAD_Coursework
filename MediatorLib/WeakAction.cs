93975900eff78b243c3e2bb03ff4067a00e855ff
Astron <astr0n@ukr.net>
2013-03-17 17:30:53 +0200

    Add Mediator pattern Lib (Copyright Marlon Grech)
    See http://marlongrech.wordpress.com/ for more details about Mediator
    Pattern

---
+++ MediatorLib/WeakAction.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Reflection;
+
+namespace MediatorLib
+{
+    /// <summary>
+    /// This class is an implementation detail of the MessageToActionsMap class.
+    /// </summary>
+    internal class WeakAction 
+    {
+        readonly MethodInfo _method;
+        readonly Type _delegateType;
+        readonly WeakReference _weakRef;
+
+        /// <summary>
+        /// Constructs a WeakAction
+        /// </summary>
+        /// <param name="target">The instance to be stored as a weak reference</param>
+        /// <param name="method">The Method Info to create the action for</param>
+        /// <param name="parameterType">The type of parameter to be passed in the action. Pass null if there is not a paramater</param>
+        internal WeakAction(object target, MethodInfo method, Type parameterType)
+        {
+            //create a Weakefernce to store the instance of the target in which the Method resides
+            _weakRef = new WeakReference(target);
+
+            _method = method;
+
+			// JAS - Added logic to construct callback type.
+			_delegateType = parameterType == null ? typeof(Action) : typeof(Action<>).MakeGenericType(parameterType);
+        }
+
+        /// <summary>
+        /// Creates a "throw away" delegate to invoke the method on the target
+        /// </summary>
+        /// <returns></returns>
+		internal Delegate CreateAction()
+		{
+			var target = _weakRef.Target;
+			if (target != null)
+			{		
+				// Rehydrate into a real Action
+				// object, so that the method
+				// can be invoked on the target.
+				return Delegate.CreateDelegate(
+							_delegateType,
+							_weakRef.Target,
+							_method);
+			}
+            return null;
+		}
+
+        /// <summary>
+        /// returns true if the target is still in memory
+        /// </summary>
+        public bool IsAlive
+        {
+            get
+            {
+                return _weakRef.IsAlive;
+            }
+        }
+    }
+}
--- WPF_GUI/ViewModels/BaseViewModel.cs
+++ WPF_GUI/ViewModels/BaseViewModel.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using WPF_GUI.Annotations;
-using WPF_GUI.Helpers;
+﻿using WPF_GUI.Helpers;
 
 namespace WPF_GUI.ViewModels
 {
